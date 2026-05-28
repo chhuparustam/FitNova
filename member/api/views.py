54a427697e1415265c60cbef64e093937e9f256e
Chhuparustam Kushwaha <chhuparustamkushwaha30@gmail.com>
2026-05-28 14:14:11 +0545

    initial commit

---
+++ member/api/views.py
@@ -0,0 +1,43 @@
+from member.models import Member
+from member.api.serializer import MemberSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+@api_view(['GET'])
+def member_list(request):
+    data = Member.objects.all()
+    serializer = MemberSerializer(data, many=True)
+    return Response(serializer.data)
+@api_view(['POST'])
+def membercreate(request):
+    post_data = request.data
+    serializer = MemberSerializer(data=post_data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message": "Member successfully created"
+        }, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+
+@api_view(['PUT'])
+def  memberupdate(request, id):
+    member = Member.objects.get(id=id)
+    serializer = MemberSerializer(member, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message": "Member successfully updated"
+        }, status=status.HTTP_200_OK)
+    else:
+        return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+@api_view(['DELETE'])
+def memberdelete(request, id):
+    member = Member.objects.filter(id=id)
+    member.delete()
+    return Response({
+        "message": "Member successfully deleted"
+    }, status=status.HTTP_204_NO_CONTENT)
